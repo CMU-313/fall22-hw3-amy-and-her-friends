ada932fe1ac7f16835f67cb397b0a1213949906a
walkingcabbages <cheng_amy@comcast.net>
2022-10-04 21:24:25 -0400

    addReview function in angular controller

--- docs-web/src/main/webapp/src/app/docs/controller/usergroup/UserGroup.js
+++ docs-web/src/main/webapp/src/app/docs/controller/usergroup/UserGroup.js
@@ -3,30 +3,53 @@
 /**
  * User/group controller.
  */
-angular.module('docs').controller('UserGroup', function(Restangular, $scope, $state) {
-  // Load users
-  Restangular.one('user/list').get({
-    sort_column: 1,
-    asc: true
-  }).then(function(data) {
-    $scope.users = data.users;
+angular
+  .module('docs')
+  .controller('UserGroup', function (Restangular, $scope, $state) {
+    // Load users
+    Restangular.one('user/list')
+      .get({
+        sort_column: 1,
+        asc: true,
+      })
+      .then(function (data) {
+        $scope.users = data.users;
+      });
+
+    // Load groups
+    Restangular.one('group')
+      .get({
+        sort_column: 1,
+        asc: true,
+      })
+      .then(function (data) {
+        $scope.groups = data.groups;
+      });
+
+    // Open a user
+    $scope.openUser = function (user) {
+      $state.go('user.profile', { username: user.username });
+    };
+
+    // Open a group
+    $scope.openGroup = function (group) {
+      $state.go('group.profile', { name: group.name });
+    };
+
+    /**
+     * Add a review.
+     */
+    $scope.addReview = function () {
+      Restangular.one('review')
+        .put($scope.review)
+        .then(function (data) {
+          $scope.reviews.push({
+            id: data.id,
+            applicant_name: $scope.review.applicantName,
+            notes: $scope.review.notes,
+            score: $scope.review.score,
+          });
+          $scope.review = { applicantName: '', notes: '', score: '1' };
+        });
+    };
   });
-
-  // Load groups
-  Restangular.one('group').get({
-    sort_column: 1,
-    asc: true
-  }).then(function(data) {
-    $scope.groups = data.groups;
-  });
-
-  // Open a user
-  $scope.openUser = function(user) {
-    $state.go('user.profile', { username: user.username });
-  };
-
-  // Open a group
-  $scope.openGroup = function(group) {
-    $state.go('group.profile', { name: group.name });
-  };
-});
