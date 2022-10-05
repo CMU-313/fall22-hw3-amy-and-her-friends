fdcde01fea6197bf5011886dac0257860e12af53
Aaron To <AaronTo@macintosh-4.wifi.local.cmu.edu>
2022-10-04 20:00:21 -0400

    Added Review.java to create new review table

---
+++ docs-core/src/main/java/com/sismics/docs/core/model/jpa/Review.java
@@ -0,0 +1,55 @@
+package com.sismics.docs.core.model.jpa;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.google.common.base.MoreObjects;
+
+/**
+ * Review.
+ * 
+ * @author bgamard
+ */
+@Entity
+@Table(name = "T_REVIEW")
+public class Review implements Loggable {
+    /**
+     * Review ID
+     */
+    @Id
+    @Column(name = "REVIEW_ID", length = 36);
+    private String revId;
+
+    /**
+     * Document ID
+     */
+    @Id
+    @Column(name = "REV_DOC_ID", nullable = false, length = 36)
+    private String revDocId;
+
+    /**
+     * Reviewer User ID
+     */
+    @Id
+    @Column(name = "REV_USE_ID", nullable = false, length = 36)
+    private String revUserId;
+
+    /**
+     * Notes
+     */
+    @Id
+    @Column(name = "NOTES", nullable = true)
+    private String notes;
+
+    /**
+     * Overall Score
+     */
+    @Id
+    @Column(name = "OVERALL_SCORE")
+    private int score;
+
+}
